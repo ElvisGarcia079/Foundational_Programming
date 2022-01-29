92bae6f93c8281725b6e8aca287d4b9931534655
Elvis Garcia <elvisgarcia93@gmail.com>
2022-01-29 11:32:42 -0500

    Add Variables with String, Number, and Boolean Data Types

---
+++ JavaScript/simpleVariables.js
@@ -0,0 +1,28 @@
+//Initializing a Variable 
+
+//In JavaScript, Declaring Variables is fairly simple: 
+
+/* 
+    1. The Keyword let or var (let is better)
+    2. A Variable Name
+    3. The Assignment Operator
+    4. The Value
+    5. A Semi Colon
+*/
+
+//The Value could be any Data Type: 
+
+//1. A String
+let progLanguage = "JavaScript";
+
+//2. A Number 
+let number = 25;
+
+//3. A Boolean 
+let isCoding = true;
+
+
+
+
+
+
