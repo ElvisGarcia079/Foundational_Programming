d15487f03cb9b790c4d095d08e2a9be92d4f8a7d
Elvis Garcia <elvisgarcia93@gmail.com>
2022-01-29 11:26:52 -0500

    Change Variable from 'name' to 'progLangauage'

--- Python/simpleVariables.py
+++ Python/simpleVariables.py
@@ -10,7 +10,7 @@
 #The Value Could Be Any Data Type: 
 
 #1. A String
-name = "Elvis"
+progLanguage = "Python"
 
 #2. A Number
 age = 28
@@ -19,16 +19,16 @@ age = 28
 isCoding = True
 
 #You can check the values by using Python's Print Method with the variable name inside
-print(name)
+print(progLanguage)
 print(age)
 print(isCoding)
 
 #You can also check the type of the data by using the Python Type Method: 
-type(name)
+type(progLanguage)
 type(age)
 type(isCoding)
 
 #If you would like to see the result of the Method Type, you will need to wrap it within the method Print
-print(type(name))
+print(type(progLanguage))
 print(type(age))
 print(type(isCoding))
