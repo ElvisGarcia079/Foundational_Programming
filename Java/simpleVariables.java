add498f2cfc9c5d90061a44fb629fbf7c3e1c05b
Elvis Garcia <elvisgarcia93@gmail.com>
2022-01-30 12:13:45 -0500

    Add Main Class File and Variable Declaration for Java 8

---
+++ Java/simpleVariables.java
@@ -0,0 +1,33 @@
+class Main{
+    public static void main(String[] args){
+        //Initializing a Variable 
+
+        //In Java 8, Declaring Variables will require some knowledge of the Data Type that you'll be using as a value to store. This is because Java 8 unlike Python and Javascript is a Compiled Language as opposed to an Interpreted Language. 
+
+        //We will be working with String, Integer, and Boolean Variables.
+
+        //To Declare Variables, we will need the following:
+        /* 
+            1. The Keyword String, int, or boolean
+                a. If the value is a String, you'll use the keyword String
+                b. If the value is a Whole Number, you'll use the keyword int
+                c. If the value is a Boolean, you'll use the keyword boolean
+            2. A Variable Name
+            3. The Assignment Operator
+            4. The Value
+            5. A Semi Colon
+        */
+
+        /* Initializing a String Variable in Java 8 */
+        String progLanguage = "Java 8";
+
+        /* Initializing a Whole Number in Java 8 */
+        int num = 21;
+
+        /* Initializing a Boolean Number in Java 8 */
+        boolean isCoding = true;
+
+        
+    }
+}
+
